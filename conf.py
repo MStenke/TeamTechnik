c6b27b85954c0619b66911c452486441f8ed96a4
MStenke <47123907+MStenke@users.noreply.github.com>
2020-03-10 17:07:34 +0100

    update

--- conf.py
+++ conf.py
@@ -172,7 +172,7 @@ html_theme_options = {
     #
     # Options are nothing (default) or the name of a valid theme
     # such as "cosmo" or "sandstone".
-    #'bootswatch_theme': "united",
+    'bootswatch_theme': "flatly",
 
     # Choose Bootstrap version.
     # Values: "3" (default) or "2" (in quotes)
